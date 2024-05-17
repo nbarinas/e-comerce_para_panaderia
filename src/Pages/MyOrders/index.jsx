3f787581a6226616b83aca2352f5764228bc6792
nbarinas <nbarinas@gmail>
2024-05-16 19:09:01 -0500

    para github

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,11 +1,29 @@
-import Layout from '../../Components/Layout'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Layout from '../../Components/Layout';
+import  OrdersCard  from '../../Components/OrdersCard';
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
+import { ShoppingCartContext } from '@heroicons/react/24/solid';
 
 function MyOrders() {
+  const context = useContext(ShoppingCartContext);
+
   return (
     <Layout>
-      MyOrders
+      <div>
+        <h1>MyOrders </h1>
+      </div>
+      
+      {context.order.map((order, index) => (
+        <Link key={index} to={`/my-orders/${order.id}`}>
+          <OrdersCard
+            totalPrice={order.totalPrice}
+            totalProducts={order.totalProducts}
+          />
+        </Link>
+      ))}
     </Layout>
-  )
+  );
 }
 
-export default MyOrders
+export default MyOrders;
